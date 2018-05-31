75874886c8cc63754f96fbade684de29b23843b7
rjain9 <rjain9@ncsu.edu>
2018-05-31 16:15:06 -0400

    Updating gulp.js and package.json

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,12 +1,12 @@
 'use strict';
 
 //dependencies
-import gulp from "gulp";
-import sass from "gulp-sass";
-import minifyCSS from "gulp-clean-css";
-import uglify from "gulp-uglify";
-import rename from "gulp-rename";
-import changed from "gulp-changed";
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var changed = require('gulp-changed');
 
 /////////////
 // - SCSS/CSS
