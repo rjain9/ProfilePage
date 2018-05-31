a2dc550652b77f63c5df622e86ecd984c8be9953
rjain9 <rjain9@ncsu.edu>
2018-05-30 21:26:07 -0400

    creating header.js and import

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+//Components
+import Header from './component/headerComponent/header'
+
+//Includes
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Test
+        <Header />
+
       </div>
     );
   }
